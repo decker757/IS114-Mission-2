3b68595c146f1093bcda9355c0b3bdcd81d6f3ca
b <66017805+bryanseah234@users.noreply.github.com>
2024-11-07 05:46:42 +0000

    edit aws read

--- scripts/reading_from_thingspeak.py
+++ scripts/reading_from_thingspeak.py
@@ -4,7 +4,7 @@ import base64
 
 def test_post(count):
     POST_URL = "https://api.thingspeak.com/update.json"
-    json_data = {'api_key':"VPS1BCH04YUJMUBJ", "field1":count}
+    json_data = {'api_key':"VPS1BCH04YUJMUBJ", "field2":count}
     response = requests.post(POST_URL, data=json_data)
     print(f"posting status {response.status_code}")
 
@@ -12,10 +12,20 @@ def get_request(url):
     # return json data if code 200, else none
     response = requests.get(url)
     if response.status_code == 200:
-        print(f"Status code {response.status_code}, Received data")
-        return response
+        # print(f"Status code {response.status_code}, Received data")
+        if response.json() == -1:
+            # no last entry
+            return None
+        
+        elif type(response.json()) == dict:
+            # returns last value
+            return response.json()
+        
+        else:
+            # unknown json data
+            return None
     else:
-        print(f"Status code {response.status_code}, Did not receive data")
+        # status code 400
         return None
     
 CHANNEL_ID = 2730808
@@ -34,44 +44,51 @@ GET_URL_FIELD_2 = f"https://api.thingspeak.com/channels/{CHANNEL_ID}/fields/2/la
 # light
 GET_URL_FIELD_3 = f"https://api.thingspeak.com/channels/{CHANNEL_ID}/fields/3/last.json?api_key={READ_API_KEY}&status=true"
 
-# start reading data from thingspeak
-count = 0
-while True:
+field1uniq = []
+field2uniq = []
+field3uniq = []
 
-    count += 1
-    test_post(count)
+# start reading data from thingspeak
+while True:
     
     # get json data
-    # field_1 = get_request(GET_URL_FIELD_1).json()
-    # print(type(field_1))
-    # field_2 = get_request(GET_URL_FIELD_2).json()
-    # print(type(field_2))
-    field_3 = get_request(GET_URL_FIELD_3).json()
-    print(field_3)
+    field1 = get_request(GET_URL_FIELD_1)
+    # print(field1)
+    field2 = get_request(GET_URL_FIELD_2)
+    # print(field2)
+    field3 = get_request(GET_URL_FIELD_3)
+    # print(field3)
     
-    # check is data is valid
+    # check is data is valid and process
 
     # {'created_at': '2024-11-07T04:58:14Z', 'entry_id': 4, 'field1': '45', 'status': None}
-    # -1
 
-    # if type(field_1) is not dict:
-    #     print('No last value detected for field 1')
-    # else:
-    #     print(field_1["field1"])
-    
-    # if type(field_2) is not dict:
-    #     print('No last value detected for field 2')
-    # else:
-    #     print(field_2["field2"])
-
-    if type(field_3) is not dict:
-        print('No last value detected for field 3')
+    if field1 is not None and field1 not in field1uniq:
+        # field 1 is dict
+        field1uniq.append(field1)
     else:
-        print(field_3["field3"])
+        #field1 is none
+        print('Unable to read field1 from thingspeak')
 
-    # process data
+    if field2 is not None and field2 not in field2uniq:
+        # field2 is dict
+        field2uniq.append(field2)
+    else:
+        # field2 is none
+        print('Unable to read field1 from thingspeak')
+
+    if field3 is not None and field3 not in field3uniq:
+        # field3 is dict
+        field3uniq.append(field3)
+    else:
+        #field3 is none
+        print('Unable to read field1 from thingspeak')
+
+    print(field3uniq)
+    print(field2uniq)
+    print(field1uniq)
 
     # append all fields to html file together
-    
+    time.sleep(30)
 
     

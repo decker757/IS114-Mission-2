0db546364321f8aaea43e4e107a5161f1d9b8cc8
b <66017805+bryanseah234@users.noreply.github.com>
2024-11-07 21:26:13 +0800

    Update reading_from_thingspeak.py

--- scripts/reading_from_thingspeak.py
+++ scripts/reading_from_thingspeak.py
@@ -1,7 +1,7 @@
 import time
 import requests
 import base64
-
+import json
 
 def get_request(url):
     # return json data if code 200, else none
@@ -20,77 +20,58 @@ def get_request(url):
             # unknown json data
             return None
 
+def format_fields_json(field1list, field2list):
+    # returns a list of dicts, to be dumped int json file
+    # len of field1list is the same as len of field2list
+    # field1 is cycle, field2 is slouch
+    r_lis = []
+    for i in range(len(field2list)):
+        data_to_save = {"cycle": field1list[i]["field1"], "slouch": field2list[i]["field2"], "timestamp": field2list[i]["created_at"]}
+        r_lis.append(data_to_save)
+    return r_lis
+
 CHANNEL_ID = 2730808
 READ_API_KEY_BASE_64 = "MzgyS09XQTJPODM1OUhLSg=="
 READ_API_KEY = base64.b64decode(READ_API_KEY_BASE_64).decode()
-API_FIELDS = {"cycle":"field1", 'slouch':"field2", "light":"field3"}
-
-# https://api.thingspeak.com/channels/9/fields/2/last.csv?api_key=E52AWRAV1RSXQQJW&status=true
+API_FIELDS = {"cycle":"field1", 'slouch':"field2"}
 
 # cycle
 GET_URL_FIELD_1 = f"https://api.thingspeak.com/channels/{CHANNEL_ID}/fields/1/last.json?api_key={READ_API_KEY}&status=true"
-
 # slouch
 GET_URL_FIELD_2 = f"https://api.thingspeak.com/channels/{CHANNEL_ID}/fields/2/last.json?api_key={READ_API_KEY}&status=true"
-
-# light
-GET_URL_FIELD_3 = f"https://api.thingspeak.com/channels/{CHANNEL_ID}/fields/3/last.json?api_key={READ_API_KEY}&status=true"
-
-def get_variable_and_value(string):
-    # returns variable value pair if the string from microbit is valid
-    try:
-        first, second = string.split(':')
-        return first, second
-    except:
-        print('Serial data not in correct format')
-        return None
         
 
-field1uniq = []
-field2uniq = []
-field3uniq = []
-# initialise serial connection
-
-print('out')
+all_field1 = []
+all_field2 = []
 
 # start reading data from thingspeak
 while True:
     
     # get json data
     field1 = get_request(GET_URL_FIELD_1)
-    # print(field1)
     field2 = get_request(GET_URL_FIELD_2)
-    # print(field2)
-    field3 = get_request(GET_URL_FIELD_3)
-    # print(field3)
-    
+
     # check is data is valid and process
     # {'created_at': '2024-11-07T04:58:14Z', 'entry_id': 4, 'field1': '45', 'status': None}
 
-    if field1 is not None and field1 not in field1uniq:
+    if field1 is not None:
         # field 1 is dict
-        field1uniq.append(field1)
+        all_field1.append(field1)
     else:
         #field1 is none
         print('Unable to read field1 from thingspeak')
 
-    if field2 is not None and field2 not in field2uniq:
+    if field2 is not None:
         # field2 is dict
-        field2uniq.append(field2)
+        all_field2.append(field2)
     else:
         # field2 is none
-        print('Unable to read field1 from thingspeak')
+        print('Unable to read field2 from thingspeak')
+    
+    dumpthis = format_fields_json(all_field1, all_field2)
+    
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(dumpthis, f)
 
-    if field3 is not None and field3 not in field3uniq:
-        # field3 is dict
-        field3uniq.append(field3)
-    else:
-        #field3 is none
-        print('Unable to read field1 from thingspeak')
-
-    print(field3uniq)
-    print(field2uniq)
-    print(field1uniq)
-
-    # append all fields to html file together
-    time.sleep(30)
+    # the javascript in the html file will read from this json file every 5 seconds
+    time.sleep(20)
